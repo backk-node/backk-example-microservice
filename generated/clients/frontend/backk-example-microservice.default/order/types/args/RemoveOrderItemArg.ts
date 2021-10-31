6b018c4307dffad344df3c9a839b5c6874bcd126
Petri Silen <petri.silen@silensoft.com>
2021-10-31 10:51:49 +0200

    PATCH: Modified

--- generated/clients/frontend/backk-example-microservice.default/order/types/args/RemoveOrderItemArg.ts
+++ generated/clients/frontend/backk-example-microservice.default/order/types/args/RemoveOrderItemArg.ts
@@ -2,7 +2,6 @@
 // This is an auto-generated file from the respective .type file
 // DO NOT MODIFY THIS FILE! Updates should be made to the respective .type file only
 // This file can be generated from the respective .type file by running npm script 'generateTypes'
-// eslint-disable-next-line @typescript-eslint/class-value-casing
 import {
   IsBigInt,
   IsStringOrObjectId,
