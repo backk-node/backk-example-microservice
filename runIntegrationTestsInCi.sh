bbdee8d66788478b7dba9485797e64ec9c01747a
Petri Silen <petri.silen@silensoft.com>
2021-12-26 11:08:38 +0200

    PATCH: Modified

--- runIntegrationTestsInCi.sh
+++
@@ -1,3 +0,0 @@
-# wait_for_it https://github.com/vishnubob/wait-for-it
-# execute curl localhost:${HTTP_SERVER_PORT}/startupCheckService.isServiceStarted with some timeout until it returns OK
-newman run postman/integration-tests-collection.json
