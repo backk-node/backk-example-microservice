3ac3163caab3178bd98bdbe29ba122f6458664d8
Petri Silen <petri.silen@silensoft.com>
2022-01-03 10:53:11 +0200

    PATCH: Modified

--- src/services/order/types/args/_IdAndOrderItemId.ts
+++ src/services/order/types/args/_IdAndOrderItemId.ts
@@ -2,6 +2,5 @@ import { _IdAndVersion } from 'backk';
 
 // eslint-disable-next-line @typescript-eslint/class-name-casing
 export default class _IdAndOrderItemId extends _IdAndVersion {
-  // NOSONAR
   orderItemId!: string;
 }
--- src/services/useraccount/types/args/_IdAndSalesItemId.ts
+++ src/services/useraccount/types/args/_IdAndSalesItemId.ts
@@ -2,6 +2,5 @@ import { _Id } from 'backk';
 
 // eslint-disable-next-line @typescript-eslint/class-name-casing
 export default class _IdAndSalesItemId extends _Id {
-  // NOSONAR
   salesItemId!: string;
 }
