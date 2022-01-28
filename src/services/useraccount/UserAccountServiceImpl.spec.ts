6b5d8d32f82d5fea275f4637baa56947cd4b7177
Petri Silen <petri.silen@silensoft.com>
2022-01-28 12:03:46 +0200

    PATCH: Changed Checkov step in CI pipeline

---
+++ src/services/useraccount/UserAccountServiceImpl.spec.ts
@@ -0,0 +1,7 @@
+describe('UserAccountServiceImpl', () => {
+  describe('followUser', () => {
+    it('should make you follow user and add you to following users of the followed user', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
