f83e974076f5ec5558b4d50fefc1738f076a20c7
Petri Silen <petri.silen@silensoft.com>
2022-02-14 17:36:13 +0200

    PATCH: Added test

--- src/services/useraccount/UserAccountServiceImpl.spec.ts
+++ src/services/useraccount/UserAccountServiceImpl.spec.ts
@@ -1,7 +1,31 @@
+import { NullDataStore } from 'backk';
+import 'reflect-metadata';
+import UserAccount from './types/entities/UserAccount';
+import UserAccountServiceImpl from './UserAccountServiceImpl';
+
 describe('UserAccountServiceImpl', () => {
   describe('followUser', () => {
     it('should make you follow user and add you to following users of the followed user', () => {
-      expect(true).toBe(true);
+      // GIVEN
+      const _id = '1';
+      const followedUserAccountId = '2';
+      const dataStore = new NullDataStore();
+      dataStore.addSubEntitiesToEntityById = jest.fn();
+      const userAccountService = new UserAccountServiceImpl(dataStore);
+
+      // WHEN
+      userAccountService.followUser({ _id, followedUserAccountId });
+
+      // THEN
+      expect(dataStore.addSubEntitiesToEntityById).toHaveBeenCalledWith(
+        'followedUserAccounts',
+        [{ _id: '2' }],
+        UserAccount,
+        '1',
+        {
+          entityPreHooks: expect.any(Function),
+        }
+      );
     });
   });
 });
